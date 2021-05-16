b4a937e0998a4b187c0941544cf87fb32bb19171
Ethical-coder <ayush.shrivastava123@gmail.com>
2021-05-16 17:14:08 +0530

    Created a model to store plasma data.

--- android_application/app/src/main/java/com/example/donors/Models/Plasma_data.kt
+++ android_application/app/src/main/java/com/example/donors/Models/Plasma_data.kt
@@ -1,15 +1,40 @@
 package com.example.donors.Models
 
-class Data{
+class plasmaData{
 
 
     var name:String="" ;
     var number : String="" ;
     var blood:String="";
-    public fun Data( name:String,number:String ,blood:String){
+    constructor( name:String,number:String ,blood:String){
         this.name=name;
         this.number=number;
         this.blood=blood;
     };
 
+    @JvmName("setName1")
+    public fun setName(name:String): Unit{
+        this.name=name;
+    }
+    @JvmName("setNumber1")
+    public fun setNumber(number: String):Unit{
+        this.number=number;
+    }
+    @JvmName("setBlood1")
+    public fun setBlood(blood: String):Unit{
+        this.blood=blood;
+    }
+
+    @JvmName("getName1")
+    public fun getName():String{
+        return this.name;
+    }
+    @JvmName("getNumber1")
+    public fun getNumber():String{
+        return this.number;
+    }
+    @JvmName("getBlood1")
+    public fun getBlood():String{
+        return this.blood;
+    }
 }
